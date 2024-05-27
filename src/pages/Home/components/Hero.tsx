959b1a19db7cd405f1ddd43d8d04987e869d8c88
GilbertoRod <gilrod1017@gmail.com>
2024-05-27 13:52:44 -0500

    change display for safari users

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import image1 from '../../../assets/hero1.jpg'
 import '../Home.css'
 import HeroVid from '../../../assets/herovid.mp4'
 
 
 function Hero() {
+  const [isSafari, setIsSafari] = useState(false);
+  useEffect(() => {
+    const ua = navigator.userAgent.toLowerCase();
+    if (ua.includes('safari') && !ua.includes('chrome')) {
+      setIsSafari(true);
+    }
+  }, []);
 
   return (
     <div className='hero-container'>
-      <video className='hero-item-1' id='vid' autoPlay muted loop playsInline>
-        <source src={HeroVid} type='video/mp4' />
-      </video>
+      <div className='video-player'>
+        {!isSafari?
+          <video className='hero-item-1' id='vid' autoPlay muted loop playsInline>
+          <source src={HeroVid} type='video/mp4' />
+        </video>
+        :
+        <img src={image1}/>
+        }
+      </div>
       <div className='hero-box'>
         <span>Your Creation</span>
         <button>Gallery</button>
