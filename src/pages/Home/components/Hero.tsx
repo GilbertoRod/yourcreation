4fe652b48b42632fbc98e1500042a041a27ccb5c
GilbertoRod <gilrod1017@gmail.com>
2024-05-27 12:56:27 -0500

    removed bug

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -5,16 +5,7 @@ import HeroVid from '../../../assets/herovid.mp4'
 
 
 function Hero() {
-  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-  if (isSafari){
-    return(
-    <div className='hero-container'>
-    <div className='hero-box'>
-      <span>Your Creation</span>
-      <button>Gallery</button>
-    </div>
-  </div>)
-  }
+
   return (
     <div className='hero-container'>
       <video className='hero-item-1' id='vid' autoPlay muted loop playsInline>
