acd30d6e73b3de46fdaad2348ec0254e71d3ed71
Gilberto Rodriguez <gilrod1017@gmail.com>
2024-05-27 02:51:22 -0500

    safari autoplay

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -7,7 +7,7 @@ import HeroVid from '../../../assets/herovid.mp4'
 function Hero() {
   return (
     <div className='hero-container'>
-      <video className='hero-item-1' autoPlay muted loop>
+      <video className='hero-item-1' autoPlay muted loop playsInline>
         {/* Nested source tag specifying the video file */}
         <source src={HeroVid} type='video/mp4' />
         Your browser does not support the video tag.
