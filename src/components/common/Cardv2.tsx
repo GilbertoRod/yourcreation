b052e12b62938272782cb602c717537f3ea49306
GilbertoRod <gilrod1017@gmail.com>
2024-05-28 10:26:58 -0500

    cardV2 Complete

--- src/components/common/Cardv2.tsx
+++ src/components/common/Cardv2.tsx
@@ -1,25 +1,23 @@
-import React, { useState } from 'react'
+
 import './Cardv2.css'
 interface Props{
     image:string
     title:string
+    desc:string
     sub:string
 }
 
-function Card({image,title,sub}: Props) {
-  const [displayInfo,setDisplayInfo]=useState(false);
+function Card({image,title,desc,sub}: Props) {
+
   return (
     <div className='card-v2-wrapper'>
         <img className="card-v2-image" src={image}/>
-        <div className={displayInfo?"card-v2-info":"card-v2-info card-v2-info-hidden"} onClick={():void=>setDisplayInfo(!displayInfo)}>
+        <div className="card-v2-info">
             <div className='card-v2-textual'>
               <div className='card-v2-head'>{title}</div>
-              <div className='card-v2-show-hide-wrapper'>
-                {!displayInfo?<p className='card-v2-show-hide-info'>see more</p>:<p className='card-v2-show-hide-info card-v2-show-hide-info-invisible'>see more</p>}
-                <div className={displayInfo?'card-v2-sub card-v2-sub-visible':'card-v2-sub'}>{sub}</div>
-              
-                {displayInfo?<p className='card-v2-show-hide-info'>see less</p>:<p className='card-v2-show-hide-info card-v2-show-hide-info-invisible'></p>}
-              </div>
+              <div className='card-v2-sub'>{sub}</div>
+
+              <div className='card-v2-desc'>{desc}</div>
               
             </div>
         </div>
