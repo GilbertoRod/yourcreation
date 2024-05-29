2b70b8d049c65d8d682314b6776d22405d91696a
Gilberto Rodriguez <gilrod1017@gmail.com>
2024-05-29 03:30:30 -0500

    about fix

--- src/pages/Home/components/About.tsx
+++ src/pages/Home/components/About.tsx
@@ -4,7 +4,7 @@ function About() {
     <>
       <div className='about-wrapper'>
           <div className='about-box'>
-              <h1>ABOUT US</h1>
+              <h1 className="home-section-title">ABOUT US</h1>
               <p>
               At Your Creation, we're more than just a business; we're dedicated creators committed to personalized service and top-notch craftsmanship. Offering Airbrush, DTG Print, Embroidery, and Vinyl customization options without minimum orders, we cater to everyone from individuals to large corporations and events. Our focus is on crafting experiences, ensuring excellence and customer satisfaction in every project. Join us today and bring your creative visions to life.
               </p>
