00898134b709572b5ccc83d5cf8b660396fa5024
GilbertoRod <gilrod1017@gmail.com>
2024-05-29 04:06:31 -0500

    resolved mac issue

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -1,40 +1,27 @@
-import React,{useEffect, useState} from 'react'
-import image1 from '../../../assets/heroAlt2.jpg'
+
 import '../Home.css'
 import HeroVid from '../../../assets/herovid.mp4'
 
 
 function Hero() {
-  const [isSafari, setIsSafari] = useState<boolean>(false);
-  useEffect(() => {
-    const ua = navigator.userAgent.toLowerCase();
-    if (ua.includes('safari') && !ua.includes('chrome')) {
-      setIsSafari(true);
-    }
-  }, []);
+
 
   return (
     <div className='hero-container'>
 
-        {!isSafari?
-        <>
+        {/*FOR NON-MAC USERS */}
         <video className='hero-item-1' id='vid' autoPlay muted loop playsInline>
           <source src={HeroVid} type='video/mp4' />
         </video>
+        
+        {/*FOR EVERY USER */}
         <div className='hero-box'>
           <span style={{fontFamily:"Rethink Sans"}}>YOUR CREATION</span>
           <button>GALLERY</button>
         </div>
-        </>
-        :
-        <>
-          <img className='hero-item-1' src={image1}/>
-          <div className='hero-box'>
-        <span className='hero-title'>Your Creation</span>
-        <button>Gallery</button>
-      </div>
-        </>
-        }
+
+
+    
       </div>
 
 
