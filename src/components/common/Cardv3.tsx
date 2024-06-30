aa7b9ef83ddeaa09dbfc4ff42a094d79e2d3a0e8
GilbertoRod <gilrod1017@gmail.com>
2024-06-29 22:28:11 -0500

    glitch fix

--- src/components/common/Cardv3.tsx
+++ src/components/common/Cardv3.tsx
@@ -13,21 +13,12 @@ function Cardv3({image,title,desc,sub,link}: Props) {
     const upperTitle=title.toUpperCase()
   return (
     <div className='card-v3-container'>
-            <div className='card-v3-img-container'>
-                <img src={image} className='card-v3-img' alt={title}/>
+            <div className='card-v3-img-container' style={{backgroundImage:`url(${image})`, backgroundPosition:"center",backgroundSize:"cover"}}>
+
                 <h1>{upperTitle}</h1>
                 <h3 className='card-v3-sub'>{sub.toUpperCase()}</h3>
                 <p className='view-gallery-btn'><Link to={link}>View {title} Gallery</Link></p>
             </div>
-            {/* <div className='card-v3-text'>
-
-                <h3>{sub.toUpperCase()}</h3>
-                <p>{desc}</p>
-                <p className='view-gallery-btn'><Link to={link}>View {title} Gallery</Link></p>
-
-            </div> */}
-
-
     </div>
   )
 }
